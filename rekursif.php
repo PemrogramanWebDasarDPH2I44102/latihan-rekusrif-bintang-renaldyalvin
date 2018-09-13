31454242ac718b8d5493f84907a864e3a57d8db0
muhammad renaldy alvianto <42663036+renaldyalvin@users.noreply.github.com>
2018-09-13 08:43:50 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,21 @@
 <?php
-
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
-}
-rekursif(5);
+	echo 'Nomor 1 : <br><br>';
+	$bts=5;
+		function rekursif ($a = 1){
+			global $bts;
+				echo '*';
+			$a++;
+			if ($a <= $bts) {
+				rekursif ($a);
+			}
+			else{ 
+				echo '<br>';
+					$bts--;
+					if ($bts !=0) {
+						rekursif ();
+				    }
+			  }
+		}
+		rekursif();
+		"*==================================================================================*"
 ?>
